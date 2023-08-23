96daedc2d64899ccf48f136b63976020cbb9b7bc
Ivan Kalashnikov <ivan@kalashnikov.uk>
2023-08-23 23:10:30 +0100

    epiphany moment

--- cronjob.sh
+++ cronjob.sh
@@ -1,3 +1,7 @@
 if ! grep -q "echo '*/5 * * * * php -f /var/www/html/cron.php" /etc/crontabs/www-data; then
     echo "echo '*/5 * * * * php -f /var/www/html/cron.php" >> /etc/crontabs/www-data
 fi
+
+# Realisation: This script will not work as it's not the host file I'm targeting but the docker container.
+# Meaning I will either need to send this script to be executed inside the container
+# Or create a docker file with nextcloud and transplant these commands inside it.
