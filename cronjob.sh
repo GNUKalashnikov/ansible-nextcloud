a9273559d91d8f510c6d2453ba84c4ccb7ecbdb2
Ivan Kalashnikov <ivan@kalashnikov.uk>
2023-08-25 09:09:56 +0100

    1-cron-job: Added a dockerfile to move the cronjob inside

--- cronjob.sh
+++ cronjob.sh
@@ -1,7 +1,3 @@
-if ! grep -q "echo '*/5 * * * * php -f /var/www/html/cron.php" /etc/crontabs/www-data; then
-    echo "echo '*/5 * * * * php -f /var/www/html/cron.php" >> /etc/crontabs/www-data
+if ! grep -q "echo */5 * * * * php -f /var/www/html/cron.php" /etc/crontab; then
+    echo "*/5 * * * * php -f /var/www/html/cron.php" >> /etc/crontab
 fi
-
-# Realisation: This script will not work as it's not the host file I'm targeting but the docker container.
-# Meaning I will either need to send this script to be executed inside the container
-# Or create a docker file with nextcloud and transplant these commands inside it.
